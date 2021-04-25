3f223bafb6271872074c7647f22fc3c452300190
mathew <mbush11@vols.utk.edu>
2021-04-25 18:36:52 -0400

    changed the GMM function to work trait by trait

--- data_analysis.py
+++ data_analysis.py
@@ -32,44 +32,54 @@ class data_analysis:
             plt.show()   
 
     # performs the actual guassian mixture model analysis
-    def GMM(self):
-        data = track_data(self.token,self.seed_track[4,0])
+    def GMM(self,trait_num):
+        features = track_data(self.token,self.seed_track[4,0])
+        means = []
+        vars = []
+
         
+        data = features[:,trait_num].reshape(-1,1)
         # this block optimizes the number of components 
-        n_comps = 27 # starts with an average if 3 components per track trait
+        n_comps = 10 # starts with 10 components
         gm_1 = GaussianMixture(n_components = n_comps,random_state=0).fit(data)
         gm_2 = GaussianMixture(n_components = n_comps + 1, random_state = 0).fit(data)
-        print('gm_1 bic: %s' % gm_1.bic(data))
-        print('gm_2 bic: %s' % gm_2.bic(data))
+        print('gm_1 bic: %s' % gm_1.aic(data))
+        print('gm_2 bic: %s' % gm_2.aic(data))
 
-        if gm_2.bic(data) > gm_1.bic(data):
-            while gm_2.bic(data) > gm_1.bic(data):
+        if gm_2.aic(data) > gm_1.aic(data):
+            while gm_2.bic(data) > gm_1.bic(data) and n_comps > 1:
                 gm_2 = gm_1
                 n_comps = n_comps - 1
                 gm_1 = GaussianMixture(n_components = n_comps, random_state = 0).fit(data)
-                print('gm_1 bic: %s' % gm_1.bic(data))
-                print('gm_2 bic: %s' % gm_2.bic(data))
+                print('gm_1 bic: %s' % gm_1.aic(data))
+                print('gm_2 bic: %s' % gm_2.aic(data))
         else: 
-            while gm_2.bic(data) < gm_1.bic(data):
+            while gm_2.aic(data) < gm_1.aic(data):
                 gm_1 = gm_2
                 n_comps = n_comps + 1
                 gm_2 = GaussianMixture(n_components = n_comps, random_state = 0).fit(data)
-                print('gm_1 bic: %s' % gm_1.bic(data))
-                print('gm_2 bic: %s' % gm_2.bic(data))
+                print('gm_1 bic: %s' % gm_1.aic(data))
+                print('gm_2 bic: %s' % gm_2.aic(data))
 
         # block that constructs the final analysis of data set
         gm_out = GaussianMixture(n_components = n_comps, random_state = 0).fit(data)
-        for i in range(0,n_comps):
-            
-            if i == 0:
-                means = gm_out.means_[i]
-                vars = gm_out.covariances_[i]
-            else:
-                means = np.vstack((means,gm_out.means_[i]))
-                vars = np.vstack((vars,gm_out.covariances_[i]))
-            
+
+
+        for j in range(0,n_comps):
+            means = np.append(means,gm_out.means_[j])
+            vars = np.append(vars,gm_out.covariances_[j])
+
+           
+                       
+        
+        
 
         return [means,vars]
+
+
+    
+        
+
 
 
 
