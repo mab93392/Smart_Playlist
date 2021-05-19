16b1b25ab70d55d5178dcb52f63325a823391fbe
mathew <mbush11@vols.utk.edu>
2021-05-18 22:26:49 -0500

    associated with express install

--- public/main.js
+++
@@ -1,8 +0,0 @@
-
-window.addEventListener('load',() => {
-    const new_user = document.querySelector('#new_user')
-
-    new_user.addEventListener('click',() => {
-        location.href = 'http://127.0.0.1:5500/auth'
-    })
-})
