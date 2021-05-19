27b3cdd4ece9c9069134e5e916f473a0fe972323
mathew <mbush11@vols.utk.edu>
2021-05-18 22:45:39 -0500

    added comments

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ window.addEventListener('load', () => {
     // establishes new user button object
     const new_user = document.querySelector('#new_user')
 
+    // redirects to authorization page
     new_user.addEventListener('click', () => {
         location.href = 'http://127.0.0.1:5500/auth'
     })
