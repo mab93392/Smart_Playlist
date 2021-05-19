3c54d5df2ecf1d7ba5c5fc97ea650ebf6dd67def
mathew <mbush11@vols.utk.edu>
2021-05-18 21:45:46 -0500

    setup to use express

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const http = require('http')
-const route = require('./route.js')
+// script that actually runs the server
 
-http.createServer(route.handleRequest).listen(5500)
+// imports needed modules
+const express = require('express')
+const app = express()
 
+// tells server to use public folder
+app.use(express.static('public'))
+
+// renders html
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+app.get('/auth', (req,res) => {
+    res.sendFile(__dirname + '/public/auth.html')
+})
+
+// listens 
+app.listen(5500, () => {
+    console.log('listening')
+})
