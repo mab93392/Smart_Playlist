f574bf329f42302cf2557eaee254e798edb04234
mathew <mbush11@vols.utk.edu>
2021-05-19 20:20:04 -0500

    added endpoints and child processes

--- server.js
+++ server.js
@@ -2,18 +2,44 @@
 
 // imports needed modules
 const express = require('express')
+const {spawn} = require('child_process')
 const app = express()
+const bodyParser = require('body-parser');
 
 // tells server to use public folder
 app.use(express.static('public'))
+
 
 // renders html
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
 })
 app.get('/auth', (req,res) => {
-    res.sendFile(__dirname + '/public/auth.html')
+    res.sendFile(__dirname + '/auth.html')
 })
+
+app.get('/auth_script', (req,res) => {
+    // initializes child process
+    const cp = spawn('python',['./jsauth.py'])
+
+    // establishes the standard output
+    cp.stdout.on('data', (data) => {
+        const ids = JSON.parse(data.toString())
+        const client = ids.client
+        const secret = ids.secret
+        
+
+    })
+
+    // closes child process
+    cp.on('close', (code) => {
+        console.log(`exited with code ${code}`)
+    })
+    console.log('works')
+    res.send()
+})
+
+
 
 // listens 
 app.listen(5500, () => {
