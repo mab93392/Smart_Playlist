303bf5567f4f35addf89a11338f962b318989132
mathew <mbush11@vols.utk.edu>
2021-05-21 14:26:23 -0500

    has client ids, ids will be refreshed

---
+++ clients.js
@@ -0,0 +1,12 @@
+// handles the client ids
+
+// normal client id 
+const client = 'e289c6a207a547529869e9d5fd47226a'
+
+// secret client id
+const secret = '44601d5e742344468a8663c7f246f5c6'
+
+
+// the exports
+exports.client = client
+exports.secret = secret
