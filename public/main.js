1617dd45bd598d3149c5a8b8f3c5bce2a8ec4b4f
mathew <mbush11@vols.utk.edu>
2021-05-21 15:28:07 -0500

    made create playlist button live

--- public/main.js
+++ public/main.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', () => {
-    // establishes new user button object
+    // establishes button objects
     const new_user = document.querySelector('#new_user')
-    
+    const create_playlist = document.querySelector('#create')
+
+    // redirects to playlist page
+    create_playlist.addEventListener('click', () => {
+        location.href = 'http://127.0.0.1:5500/playlist'
+    })
+
     // redirects to authorization page
     new_user.addEventListener('click', () => {
         location.href = 'http://127.0.0.1:5500/auth'
